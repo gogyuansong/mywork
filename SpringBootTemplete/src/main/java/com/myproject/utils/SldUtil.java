ca4f5e403522eb9d42fac1da7746f63ce0128332
gys <835089155@qq.com>
2017-12-26 21:49:28 +0800

    提交代码

--- SpringBootTemplete/src/main/java/com/myproject/utils/SldUtil.java
+++ SpringBootTemplete/src/main/java/com/myproject/utils/SldUtil.java
@@ -1,11 +1,11 @@
 package com.myproject.utils;
 
-import org.geotools.factory.CommonFactoryFinder;
-import org.geotools.styling.StyleFactory;
+/*import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.styling.StyleFactory;*/
 
 public class SldUtil {
 
-	public static void createSld(){
+/*	public static void createSld(){
 		StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory();
-	}
+	}*/
 }
